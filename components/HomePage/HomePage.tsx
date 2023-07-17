3edeeb45350b8c2228253837f7e80969940ae8b1
arslan759 <arslan.obaid@codistan.org>
2023-07-17 19:06:22 +0500

    add: homepage (incomplete) UI

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -4,17 +4,25 @@ import HowItWorks from '../HowItWorks/HowItWorks'
 import OurGallery from '../OurGallery/OurGallery'
 import OurMission from '../OurMission/OurMission'
 import ClientsSay from '../ClientsSay/ClientsSay'
+import SwiperJS from '../SwiperJS'
 import FAQs from '../FAQs/FAQs'
 
 const HomePage = () => {
   return (
     <div>
-      <div className={styles.heroSection}>
-        <Navbar />
+      <div className='relative'>
+        <div className='absolute top-0 z-10'>
+          <div className='w-[99vw] relative'>
+            <Navbar />
+          </div>
+        </div>
+        <SwiperJS />
       </div>
 
       {/* How It Works Section */}
-      <HowItWorks />
+      <div className={`w-full ${styles.HowItWorksSection}`}>
+        <HowItWorks />
+      </div>
 
       {/* Our Gallery Section */}
       <OurGallery />
