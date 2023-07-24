6dd640bfa4f30468f1eb98d504c9bb0198cc9f83
muhammadirtaza <muhammad.irtaza@codistan.org>
2023-07-24 12:50:08 +0500

    feat: added authentication

--- components/ResetPassword/ResetPassword.tsx
+++ components/ResetPassword/ResetPassword.tsx
@@ -1,12 +1,19 @@
+import React, { FC } from 'react'
 import ResetPasswordForm from '../ResetPasswordForm'
 import Navbar from '../NavBar/NavBar'
 import styles from './styles.module.css'
 
-export default function ForgetPassword() {
+interface ForgetPasswordProps {
+  otp: number // Change `any` to the appropriate type for `otp`
+}
+
+const ForgetPassword: React.FC<ForgetPasswordProps> = ({ otp }) => {
   return (
     <div className={`${styles.forgetpassword} pb-[180px] md:h-[1072px] md:bg-cover md:bg-center`}>
       <Navbar itemsColor='white' />
-      <ResetPasswordForm />
+      <ResetPasswordForm otp={parseInt(otp)} />
     </div>
   )
 }
+
+export default ForgetPassword
