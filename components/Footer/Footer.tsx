cd5ee67f35fc46de960cb220dcdf6ac29081aa65
arslan759 <arslan.obaid@codistan.org>
2023-08-22 15:56:22 +0500

    add: version in footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import FooterMobile from '../FooterMobile/FooterMobile'
 
 export default function Footer() {
   return (
-    <div>
+    <div className='relative'>
       {/* Desktop View */}
       <div className='hidden md:block'>
         <FooterDesktop />
@@ -16,6 +16,7 @@ export default function Footer() {
       </div>
       <Typography variant='body1' className='text-center py-[16px] w-full bg-[#FFD9E4]'>
         Copyright © 2023 Your Baking Connection. All Rights Reserved
+        <span className='absolute right-3'>v1.0</span>
       </Typography>
     </div>
   )
