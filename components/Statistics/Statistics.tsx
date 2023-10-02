e99343a2d371d5d04503d5644976a0d6ea39633a
Bilal Isa Khan <bilalisakhan97@gmail.com>
2023-10-02 18:00:45 +0500

    add: reviews complete

---
+++ components/Statistics/Statistics.tsx
@@ -0,0 +1,14 @@
+const Statistics = () => {
+  return (
+    <div className='flex'>
+      <div className='flex'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <div>col2</div>
+    </div>
+  )
+}
+
+export default Statistics
